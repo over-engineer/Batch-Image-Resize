7fc5ab8bad8f38ac5cb0e33774aba89a0c1461e7
dn0z <dn0zemail@gmail.com>
2016-11-09 19:55:53 +0200

    Added confirmation dialog and documented some functions/methods

--- bir.py
+++ bir.py
@@ -7,22 +7,61 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
+from tkinter import messagebox
 
 
 class Application(Frame):
     def set_properties_defaults(self):
+        """
+        Set the default values for our class properties
+        """
+
         self.selected_directory.set("No directory selected")
         self.export_properties["width"].set("width px")
         self.export_properties["height"].set("height px")
         self.export_properties["type"].set("PNG")   # default export type
-        self.overwrite_original.set(0)              # default is disabled
+        self.overwrite_original.set(False)          # default is disabled
+
+    def confirm_settings(self):
+        """
+        Display a confirmation dialog with the current settings
+
+        :return:    whether we want to continue exporting or cancel it
+                    Return `True` if we want to export, or `False` if we don't
+        """
+
+        confirm_msg = "You are about to export with these settings:"
+        confirm_msg += "\nDirectory: " + self.selected_directory.get()
+        confirm_msg += "\nResize images to: " + self.export_properties["width"].get()
+        confirm_msg += "x" + self.export_properties["height"].get() + " px"
+        confirm_msg += "\nOverwrite original: "
+        if self.overwrite_original:
+            confirm_msg += "YES"
+        else:
+            confirm_msg += "NO"
+        confirm_msg += "\nSave as: " + self.export_properties["type"].get()
+        confirm_msg += "\n\nAre you sure you want to continue?"
+
+        return messagebox.askyesno("Export confirmation", confirm_msg)
+
+    def export_button_handler(self):
+        """
+        The handler of the Export button
+        """
+
+        print(self.confirm_settings())
 
     def browse_for_directory(self):
+        """
+        Browse using `filedialog` for the images directory and store it at `self.selected_directory`
+        Gets called when we click the Browse button
+        """
+
         self.selected_directory.set(filedialog.askdirectory())
 
     def create_widgets(self):
         """
-        Create the UI widgets
+        Create the UI elements
         """
 
         # Fonts
@@ -41,9 +80,7 @@ class Application(Frame):
         browse_field = ttk.Entry(main_container, textvariable=self.selected_directory)
         browse_field.grid(row=1, column=0, columnspan=2, sticky="we", padx=2)
 
-        browse_btn = ttk.Button(main_container)
-        browse_btn["text"] = "Browse"
-        browse_btn["command"] = lambda: self.browse_for_directory()
+        browse_btn = ttk.Button(main_container, text="Browse", command=self.browse_for_directory)
         browse_btn.grid(row=1, column=2, sticky="we", padx=2)
 
         # Resize to
@@ -71,13 +108,12 @@ class Application(Frame):
         overwrite_checkbox = ttk.Checkbutton(main_container,
                                              text="Overwrite original",
                                              variable=self.overwrite_original,
-                                             onvalue=1,
-                                             offvalue=0)
+                                             onvalue=True,
+                                             offvalue=False)
         overwrite_checkbox.grid(row=4, column=0, columnspan=2, sticky="w")
 
         # Export
-        export_btn = ttk.Button(main_container)
-        export_btn["text"] = "Export"
+        export_btn = ttk.Button(main_container, text="Export", command=self.export_button_handler)
         export_btn.grid(row=3, column=2, rowspan=2, sticky="nesw", padx=2)
 
         # Copyright
@@ -94,7 +130,7 @@ class Application(Frame):
 
         # properties
         self.selected_directory = StringVar(self)
-        self.overwrite_original = IntVar(self)
+        self.overwrite_original = BooleanVar(self)
         self.export_properties = {
             "width": StringVar(self),
             "height": StringVar(self),
@@ -108,6 +144,10 @@ class Application(Frame):
 
 
 def main():
+    """
+    The main function
+    """
+
     root = Tk()
 
     root.title("Batch Image Resize")
