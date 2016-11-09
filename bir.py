74505a1d1d78d03ef2ceb61281dde56c277f39fe
dn0z <dn0zemail@gmail.com>
2016-11-09 21:30:28 +0200

    Added the opening, resizing and saving logic

--- bir.py
+++ bir.py
@@ -5,7 +5,7 @@
 # https://github.com/dn0z/Batch-Image-Resize
 
 import os
-
+from PIL import Image
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
@@ -21,8 +21,8 @@ class Application(Frame):
         self.selected_directory.set("No directory selected")
         self.export_properties["width"].set("width px")
         self.export_properties["height"].set("height px")
-        self.export_properties["type"].set("PNG")   # default export type
-        self.overwrite_original.set(False)          # default is disabled
+        self.export_properties["type"].set("PNG")  # default export type
+        self.overwrite_original.set(False)  # default is disabled
 
     def confirm_settings(self):
         """
@@ -37,7 +37,7 @@ class Application(Frame):
         confirm_msg += "\nResize images to: " + self.export_properties["width"].get()
         confirm_msg += "x" + self.export_properties["height"].get() + " px"
         confirm_msg += "\nOverwrite original: "
-        if self.overwrite_original:
+        if self.overwrite_original.get():
             confirm_msg += "YES"
         else:
             confirm_msg += "NO"
@@ -46,8 +46,81 @@ class Application(Frame):
 
         return messagebox.askyesno("Export confirmation", confirm_msg)
 
-    def export(self):
-        print("Exporting")
+    def is_image(self, filename):
+        """
+        Checks if a filename is an image (png, jpg or jpeg, case insensitive)
+
+        :param filename: The filename (as a string)
+        :return: `True` if the given filename is an image, or `False` if it's not
+        """
+
+        file = filename.lower()
+        return file.endswith(".png") or file.endswith(".jpg") or file.endswith(".jpeg")
+
+    def get_filename_with_type(self, filename, file_type, suffix=""):
+        """
+        Get a filename and return it with the given suffix and the correct file extension for the given type
+
+        :param filename: The filename (e.g. "image_file.jpg")
+        :param file_type: The file type (e.g. "PNG")
+        :param suffix: An optional string to place between the name and the extension of the file (default is "")
+        :return: the filename with the correct extension for the given type (e.g. "image_file.png")
+        """
+
+        extension = filename.split(".")[-1]
+        return filename[:-len(extension)] + suffix + "." + file_type.lowercase()
+
+    def export_file(self, path, name, width, height, export_type, overwrite):
+        """
+        Open, resize and save an image with the given properties
+
+        :param path: The path to the directory where the image is located (without the image filename)
+        :param name: The filename of the image we want to export
+        :param width: The new width we want to resize to
+        :param height: The new height we want to resize to
+        :param export_type: The file type we want to save to (we ignore it if `overwrite` is `True`)
+        :param overwrite: Whether we want to overwrite the original files or not
+        """
+
+        img_path = os.path.join(path, name)
+
+        # open the given image and resize it
+        img = Image.open(img_path)
+        img = img.resize((width, height), Image.ANTIALIAS)
+
+        # set the destination image file we want to save
+        dest_img_name = self.get_filename_with_type(name, export_type, "_resized")
+        if overwrite:
+            dest_img_name = name
+
+        # save the resized/converted image
+        print("Save to " + os.path.join(path, dest_img_name))
+        # img.save(os.path.join(path, dest_img_name))
+
+    def init_export(self):
+        """
+        Export all the images in the selected directory
+
+        When `self.init_export()` is called, everything is ready to resize and export images
+        This loops through all the files in the given directory and calls `self.export_file()`
+        for the actual opening, resizing and saving of the files
+        """
+
+        # final export settings
+        directory_path = self.selected_directory.get()
+        width = int(self.export_properties["width"].get())
+        height = int(self.export_properties["height"].get())
+        export_type = self.export_properties["type"].get()
+        overwrite = self.overwrite_original.get()
+
+        # loop through the files in the given directory and export any image files
+        for path, subdirs, files in os.walk(directory_path):
+            for name in files:
+                if self.is_image(name):
+                    self.export_file(path, name, width, height, export_type, overwrite)
+
+        # at this point, we are done with our exports, display a success message
+        messagebox.showinfo("Exports completed", "All images were exported successfully")
 
     def export_button_handler(self):
         """
@@ -68,9 +141,16 @@ class Application(Frame):
                                      "The directory \"" + selected_directory + "\" does not exist")
             else:
                 # Directory exists
-                if self.confirm_settings():
-                    # Settings confirmed, we are ready to export
-                    self.export()
+                if (not self.export_properties["width"].get().isdigit()) or \
+                        (not self.export_properties["height"].get().isdigit()):
+                    # Dimensions are not digits
+                    messagebox.showerror("Invalid dimensions",
+                                         "Width and height must be integers")
+                else:
+                    # Dimensions are digits
+                    if self.confirm_settings():
+                        # Settings confirmed, we are ready to export
+                        self.init_export()
 
     def browse_for_directory(self):
         """
@@ -79,6 +159,19 @@ class Application(Frame):
         """
 
         self.selected_directory.set(filedialog.askdirectory())
+
+    def toggle_save_as_dropdown(self):
+        """
+        Change the state of `self.save_as_dropdown` based on `self.overwrite_original`
+
+        The dropdown should be enabled when `self.overwrite_original` is set to `False`
+        and disabled when `self.overwrite_original` is set to `True`
+        """
+
+        if self.overwrite_original.get():
+            self.save_as_dropdown.configure(state="disabled")
+        else:
+            self.save_as_dropdown.configure(state="enabled")
 
     def create_widgets(self):
         """
@@ -118,19 +211,20 @@ class Application(Frame):
         save_as_label = Label(main_container, text="Save as:", font=font_medium)
         save_as_label.grid(row=3, column=0, sticky="e")
 
-        save_as_dropdown = ttk.OptionMenu(main_container,
+        self.save_as_dropdown = ttk.OptionMenu(main_container,
                                           self.export_properties["type"],
                                           self.export_properties["type"].get(),
                                           "PNG",
                                           "JPEG")
-        save_as_dropdown.grid(row=3, column=1, sticky="we", padx=2)
+        self.save_as_dropdown.grid(row=3, column=1, sticky="we", padx=2)
 
         # Overwrite original
         overwrite_checkbox = ttk.Checkbutton(main_container,
                                              text="Overwrite original",
                                              variable=self.overwrite_original,
                                              onvalue=True,
-                                             offvalue=False)
+                                             offvalue=False,
+                                             command=self.toggle_save_as_dropdown)
         overwrite_checkbox.grid(row=4, column=0, columnspan=2, sticky="w")
 
         # Export
@@ -150,6 +244,8 @@ class Application(Frame):
         Frame.__init__(self, parent)
 
         # properties
+        self.save_as_dropdown = None
+
         self.selected_directory = StringVar(self)
         self.overwrite_original = BooleanVar(self)
         self.export_properties = {
