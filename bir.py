af9b6f842f766cee3452d0e441d1553c57937283
dn0z <dn0zemail@gmail.com>
2016-11-09 18:46:34 +0200

    Improved UI (especially padx)

--- bir.py
+++ bir.py
@@ -6,10 +6,14 @@
 
 from tkinter import *
 from tkinter import ttk
+from tkinter import filedialog
 
 
 class Application(Frame):
-    def create_widgets(self, parent):
+    def browse_for_directory(self):
+        images_dir_path = filedialog.askdirectory()
+
+    def create_widgets(self):
         """
         Create the UI widgets
         """
@@ -24,15 +28,16 @@ class Application(Frame):
 
         # Header/title
         label = Label(main_container, text="Batch Image Resize", font=font_big)
-        label.grid(row=0, column=0, columnspan=3, pady=10)
+        label.grid(row=0, column=0, columnspan=3, pady=8)
 
         # Browse
         browse_field = ttk.Entry(main_container)
         browse_field.insert(0, "No directory selected")
-        browse_field.grid(row=1, column=0, columnspan=2, sticky="we")
+        browse_field.grid(row=1, column=0, columnspan=2, sticky="we", padx=2)
 
         browse_btn = ttk.Button(main_container)
         browse_btn["text"] = "Browse"
+        browse_btn["command"] = lambda: self.browse_for_directory
         browse_btn.grid(row=1, column=2, sticky="we", padx=2)
 
         # Resize to
@@ -40,17 +45,19 @@ class Application(Frame):
         resize_to_label.grid(row=2, column=0, sticky="e")
 
         resize_width_field = ttk.Entry(main_container)
+        resize_width_field.insert(0, "width px")
         resize_width_field.grid(row=2, column=1, padx=2)
 
         resize_height_field = ttk.Entry(main_container)
-        resize_height_field.grid(row=2, column=2, padx=2)
+        resize_height_field.insert(0, "height px")
+        resize_height_field.grid(row=2, column=2, padx=3)
 
         # Save as
         save_as_label = Label(main_container, text="Save as:", font=font_medium)
         save_as_label.grid(row=3, column=0, sticky="e")
 
         save_as_dropdown = ttk.OptionMenu(main_container, self.export_type, self.export_type.get(), "PNG", "JPEG")
-        save_as_dropdown.grid(row=3, column=1, sticky="we")
+        save_as_dropdown.grid(row=3, column=1, sticky="we", padx=2)
 
         # Overwrite original
         overwrite_checkbox = ttk.Checkbutton(main_container,
@@ -63,37 +70,41 @@ class Application(Frame):
         # Export
         export_btn = ttk.Button(main_container)
         export_btn["text"] = "Export"
-        export_btn.grid(row=3, column=2, rowspan=2, sticky="nesw")
+        export_btn.grid(row=3, column=2, rowspan=2, sticky="nesw", padx=2)
 
         # Copyright
         copyright_label = Label(main_container, text="Copyright (c) 2016 dn0z | v0.1", font=font_small)
         copyright_label.grid(row=5, column=0, columnspan=3, pady=20)
 
-    def __init__(self, master=None):
+    def __init__(self, parent=None):
         """
         The constructor of the Application class
         """
 
-        Frame.__init__(self, master)
+        # super class call
+        Frame.__init__(self, parent)
+
+        # properties
         self.export_type = StringVar(self)
         self.export_type.set("PNG")             # default export type
         self.overwrite_original = IntVar(self)
         self.overwrite_original.set(0)          # default is disabled
 
+        # call methods
         self.grid()
-        self.create_widgets(master)
+        self.create_widgets()
 
 
 def main():
     root = Tk()
 
-    root.title("Hello world")
+    root.title("Batch Image Resize")
     root.geometry("360x210")
 
     style = ttk.Style()
     style.theme_use("vista")
 
-    app = Application(master=root)
+    app = Application(parent=root)
     app.mainloop()
 
 
